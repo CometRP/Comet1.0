f9e52f940970a6161bc4b10444af4045a6313526
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-09-19 12:36:24 +0930

    status stuff never finished just a general idea

---
+++ resources/[core]/comet-base/client/components/cl_status.lua
@@ -0,0 +1,26 @@
+Components.Status = {}
+
+Shared.Status = {
+    
+}
+
+Components.Status.Test = function()
+    return true
+end
+
+Components.Status.Get = function()
+    return true
+end
+
+Components.Status.Set = function()
+    return true
+end
+
+-- Components.Status.Armor = function()
+--     local self = {}
+--     self.Get = {}
+--     self.Set = {}
+
+
+--     return self
+-- end
