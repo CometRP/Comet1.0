29ced65c857b7f31545a5435840713b611994ec7
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-09-25 23:35:40 +0930

    prison and hospital maps thank god

---
+++ resources/[assets]/comet_hospital/__resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+-------------------------------STAR LEAKS
+-----------ANAKIN#6129---------STAR LEAKS
+-----------elfeedoo#6093-------STAR LEAKS
+-----------Kozaczek#0299-------STAR LEAKS
+--https://discord.gg/xTHKYXN---STAR LEAKS
+-------------------------------STAR LEAKS
